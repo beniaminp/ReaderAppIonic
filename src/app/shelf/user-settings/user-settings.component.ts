cbdb94a8cba67a18bd9b8d8628640f6c209f9931
Dragos-Beniamin Pantiru <beniaminpantiru@gmail.com>
2019-09-19 17:43:28 +0300

    added logout

--- src/app/shelf/user-settings/user-settings.component.ts
+++ src/app/shelf/user-settings/user-settings.component.ts
@@ -3,6 +3,7 @@ import {HttpParseService} from "../../services/http-parse.service";
 import {AppStorageService} from "../../services/app-storage.service";
 import {PopoverController} from "@ionic/angular";
 import {UserDTO} from "../../models/UserDTO";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-user-settings',
@@ -14,7 +15,8 @@ export class UserSettingsComponent implements OnInit {
 
     constructor(private httpParseService: HttpParseService,
                 private appStorageService: AppStorageService,
-                private popoverController: PopoverController) {
+                private popoverController: PopoverController,
+                private router: Router) {
     }
 
     ngOnInit() {
@@ -28,8 +30,15 @@ export class UserSettingsComponent implements OnInit {
         )
     }
 
-    setOpenLastRead() {
+    async setOpenLastRead() {
         this.userDTO.goToLastRead = !this.userDTO.goToLastRead;
         this.httpParseService.updateOpenLastRead(this.userDTO);
+        await this.popoverController.dismiss();
+    }
+
+    async logOut() {
+        await this.popoverController.dismiss();
+        this.appStorageService.setUserDTO(null).then();
+        this.router.navigate(['/auth/login']).then();
     }
 }
