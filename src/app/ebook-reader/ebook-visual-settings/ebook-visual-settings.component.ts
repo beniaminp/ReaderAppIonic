b59830555316338b87da1fbdb98c8341e57d0bdd
Dragos-Beniamin Pantiru <beniaminpantiru@gmail.com>
2019-09-24 17:03:49 +0300

    fixed font size

--- src/app/ebook-reader/ebook-visual-settings/ebook-visual-settings.component.ts
+++ src/app/ebook-reader/ebook-visual-settings/ebook-visual-settings.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {EBookService, EPUB_EVENT_TYPES} from "../services/e-book.service";
 import {ModalController} from "@ionic/angular";
+import {AppStorageService} from "../../services/app-storage.service";
+import {UserDTO} from "../../models/UserDTO";
 
 @Component({
     selector: 'app-ebook-visual-settings',
@@ -12,13 +14,19 @@ export class EbookVisualSettingsComponent implements OnInit {
     public selectedFont = 100;
 
     constructor(private eBookService: EBookService,
-                public modalController: ModalController) {
+                public modalController: ModalController,
+                public appStorageService: AppStorageService) {
     }
 
     ngOnInit() {
-        for (let i = 10; i < 200; i += 10) {
+        for (let i = 10; i <= 200; i += 10) {
             this.fontSize.push(i);
         }
+        this.appStorageService.getUserDTO().then(
+            (userDTO: UserDTO) => {
+                this.selectedFont = userDTO.fontSize;
+            }
+        )
     }
 
     public selectionChanged(event) {
